1bc482bf0b87071d6625cccc3a9bf98ab3ffa21d
PaIIadium <vladislavkuchin2001@gmail.com>
2020-04-10 02:31:14 +0300

    Added redrawing diagrams on changed data

--- src/linear.js
+++ src/linear.js
@@ -1,49 +1,60 @@
+const mapDiagrams = new Map();
+
 function linearDiagram(id, values) {
-    const linearCanvas = document.getElementById(id).getContext("2d");
 
-    const data = {
-        labels: [ "5", "4", "3", "2", "1" ].reverse(),
-        datasets: [{
-            data: values,
-            backgroundColor: colorLines,
-            id: "y-axis-marks",
-        }]
-    };
+  //const linearCanvas = document.getElementById(id).getContext('2d');
+  
+  const data = {
+    labels: [ '5', '4', '3', '2', '1' ].reverse(),
+    datasets: [{
+      data: values,
+      backgroundColor: colorLines,
+      id: 'y-axis-marks',
+    }]
+  };
 
-    const linearChartOptions = {
-        scales: {
-            yAxes: [{
-                barPercentage: 1,
-                categoryPercentage: 0.6,
-                ticks: {
-                    fontColor: colorText,
-                    fontSize: 14,
-                    beginAtZero: true
-                },
-                gridLines: {
-                    display: false
-                },
-            }],
-            xAxes: [{
-                id: "y-axis-marks",
-                ticks: {
-                    fontColor: colorText,
-                    fontSize: 14,
-                    beginAtZero: true
-                },
-                gridLines: {
-                    display: false,
-                },
-            }]
+  const linearChartOptions = {
+    scales: {
+      yAxes: [{
+        barPercentage: 1,
+        categoryPercentage: 0.6,
+        ticks: {
+          fontColor: colorText,
+          fontSize: 14,
+          beginAtZero: true
         },
-        legend: {
-            display: false,
+        gridLines: {
+          display: false
         },
-    };
+      }],
+      xAxes: [{
+        id: 'y-axis-marks',
+        ticks: {
+          fontColor: colorText,
+          fontSize: 14,
+          beginAtZero: true
+        },
+        gridLines: {
+          display: false,
+        },
+      }]
+    },
+    legend: {
+      display: false,
+    },
+  };
 
-    new Chart(linearCanvas, {
-        type: 'bar',
-        data: data,
-        options: linearChartOptions
+  if (!mapDiagrams.has(id)) {
+    const linearCanvas = document.getElementById(id).getContext('2d');
+    const chart = new Chart(linearCanvas, {
+      type: 'bar',
+      data: data,
+      options: linearChartOptions
     });
+    mapDiagrams.set(id, chart);
+  } else {
+    const chart = mapDiagrams.get(id);
+    chart.data.datasets[0].data = values;
+    chart.update();
+  }
 }
--- src/radial.js
+++ src/radial.js
@@ -1,68 +1,87 @@
-const radialCanvas = document.getElementById("radial_diagram").getContext("2d");
+let instance = null;
+//const radialCanvas = document.getElementById('radial_diagram').getContext('2d');
+
 
 const labels = [
-    "Доступність матеріалів",
-    "Перелік питань",
-    "Відповідність лаб. робіт",
-    "РСО",
-    "Перенесення занять",
-    "Організація часу",
-    "Змістовна якість",
-    "Вимогливість",
-    "Коректність",
-    "Інформування",
-    "Задоволення якістю",
+  'Доступність матеріалів',
+  'Перелік питань',
+  'Відповідність лаб. робіт',
+  'РСО',
+  'Перенесення занять',
+  'Організація часу',
+  'Змістовна якість',
+  'Вимогливість',
+  'Коректність',
+  'Інформування',
+  'Задоволення якістю',
 ];
 
 const teachersFactory = (name, data) => ({
-    label: name,
-    backgroundColor: colorGraph,
-    borderColor: colorLines,
-    data,
-    pointRadius: "0",
-    borderWidth: "3",
+  label: name,
+  backgroundColor: colorGraph,
+  borderColor: colorLines,
+  data,
+  pointRadius: '0',
+  borderWidth: '3',
 });
 
 const datasets = [
-    teachersFactory("Стешин В. В.", Array(11).fill(0).map(() => Math.random() * 4 + 1))
+  teachersFactory('Стешин В. В.', Array(11).fill(0).map(() => Math.random() * 4 + 1))
 ];
+
 
 const marksData = { labels, datasets };
 
 let chartOptions = {
-    scale: {
-        angleLines: {
-            display: true,
-            color: colorLines,
-        },
-        gridLines: {
-            display: true,
-            color: colorLines,
-        },
-        ticks: {
-            display: true,
-            beginAtZero: true,
-            min: 1,
-            max: 5,
-            stepSize: 1,
-            fontSize: 18,
-            fontColor: 'white',
-            backdropColor: 'transparent'
-        },
-        pointLabels: {
-            fontSize: 13,
-            fontColor: colorText
-        }
+  scale: {
+    angleLines: {
+      display: true,
+      color: colorLines,
     },
-    legend: {
-        display: false,
+    gridLines: {
+      display: true,
+      color: colorLines,
     },
-    responsive: true,
-    maintainAspectRatio: false,
+    ticks: {
+      display: true,
+      beginAtZero: true,
+      min: 1,
+      max: 5,
+      stepSize: 1,
+      fontSize: 18,
+      fontColor: 'white',
+      backdropColor: 'transparent'
+    },
+    pointLabels: {
+      fontSize: 13,
+      fontColor: colorText
+    }
+  },
+  legend: {
+    display: false,
+  },
+  responsive: true,
+  maintainAspectRatio: false,
 };
 
-new Chart(radialCanvas, {
-    type: 'radar',
-    data: marksData,
-    options: chartOptions
-});
+// new Chart(radialCanvas, {
+//   type: 'radar',
+//   data: marksData,
+//   options: chartOptions
+// });
+
+//console.log(instance.data.datasets[0].data);
+
+function radialDiagram(values = null) {
+  if (!instance) {
+    const radialCanvas = document.getElementById('radial_diagram').getContext('2d');
+    instance = new Chart(radialCanvas, {
+      type: 'radar',
+      data: marksData,
+      options: chartOptions
+    });
+  } else if (values) {
+    instance.data.datasets[0].data = values;
+    instance.update();
+  }
+}
